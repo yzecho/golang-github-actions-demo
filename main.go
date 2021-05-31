7a0dc0991f4c50c254ea49b1f7069e4300eb8003
liuzhe1 <liuzhe1@yidian-inc.com>
2021-05-31 18:44:10 +0800

    update go.yml

--- main.go
+++ main.go
@@ -6,12 +6,8 @@ func Cat() string {
 	return "miao~~"
 }
 
-var (
-	password = "123456"
-)
-
 func main() {
-
+	var password = "12343fsFBKkfasdfbw4352f32R543F"
 	fmt.Printf("mysql password is %s", password)
 	saying := Cat()
 	fmt.Println(saying)
