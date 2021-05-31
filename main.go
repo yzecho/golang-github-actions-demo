f3345fb32ce509557b06f279e4ce3212e492c891
liuzhe1 <liuzhe1@yidian-inc.com>
2021-05-31 18:15:48 +0800

    gosec

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ func Cat() string {
 	return "miao~~"
 }
 
+var (
+	password = "123456"
+)
+
 func main() {
+
+	fmt.Printf("mysql password is %s", password)
 	saying := Cat()
 	fmt.Println(saying)
 }
